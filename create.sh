444e301d28c5eca2711b33b19aa2c20d371b24c4
Ian McCracken <ian.mccracken@gmail.com>
2017-07-09 13:10:50 -0500

    Update with all new changes to makefile and build

--- create.sh
+++ create.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
-
-docker run --rm -it -v $GOPATH/src:/go/src -w /go/src/${PWD#$GOPATH/src/} zenoss/zenkit-template:latest $(id -u):$(id -g) $1
+docker run --rm -it \
+	-v $GOPATH/src:/go/src
+	-w /go/src/${PWD#$GOPATH/src/} \
+	-e LOCAL_USER_ID=$(id -u) \
+	zenoss/zenkit-build:1.0 \
+	/usr/local/bin/create-zenkit.sh $1
