1c5d9bc70211a28e443b74efc757d323e4c62d43
Summer Mousa <smousa@zenoss.com>
2017-07-28 14:46:33 -0500

    added a detailed example

--- template/hooks/{{Name}}_hooks.go
+++ template/hooks/{{Name}}_hooks.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"strconv"
 	"strings"
 
 	"github.com/snikch/goodman/hooks"
@@ -14,6 +17,20 @@ func main() {
 	// skip admin calls
 	h.BeforeEach(func(t *trans.Transaction) {
 		t.Skip = strings.HasPrefix(t.Name, "admin >")
+	})
+
+	// a detailed example
+	h.After("example > /add/{a}/{b} > add example > 200 > application/x.tester.sum+json", func(t *trans.Transaction) {
+		parts := strings.Split(t.FullPath, "/")
+		a, _ := strconv.Atoi(parts[2])
+		b, _ := strconv.Atoi(parts[3])
+
+		actual := &app.X{{Name | title}}Sum{}
+		_ = json.Unmarshal([]byte(t.Real.Body), actual)
+
+		if total := a + b; total != actual.Total {
+			t.Fail = fmt.Sprintf("expected (%d) and actual (%d) values do not match", total, actual.Total)
+		}
 	})
 
 	// add additional hooks here
