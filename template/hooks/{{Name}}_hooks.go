5ecf65eef2392901f4b288a8226a35d39ef21e29
Summer Mousa <smousa@zenoss.com>
2017-07-28 13:53:51 -0500

    skip admin tests better

--- template/hooks/{{Name}}_hooks.go
+++ template/hooks/{{Name}}_hooks.go
@@ -1,27 +1,20 @@
 package main
 
 import (
+	"strings"
+
 	"github.com/snikch/goodman/hooks"
 	trans "github.com/snikch/goodman/transaction"
 )
-
-var AdminAPI = []string{
-	"admin > /_admin/metrics > metrics admin > 200 > application/json",
-	"admin > /_admin/ping > ping admin > 200",
-	"admin > /_admin/swagger > swagger admin > 200",
-	"admin > /_admin/swagger/swagger.json > swagger.json admin > 200 > application/json",
-}
 
 func main() {
 	h := hooks.NewHooks()
 	server := hooks.NewServer(hooks.NewHooksRunner(h))
 
 	// skip admin calls
-	for _, api := range AdminAPI {
-		h.Before(api, func(t *trans.Transaction) {
-			t.Skip = true
-		})
-	}
+	h.BeforeEach(func(t *trans.Transaction) {
+		t.Skip = strings.HasPrefix(t.Name, "admin >")
+	})
 
 	// add additional hooks here
 
