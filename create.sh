d955df270aa53d7ddb213eac366f4c5668e6d6c1
Summer Mousa <smousa@zenoss.com>
2017-07-31 10:48:02 -0500

    bump zenkit-build version

--- create.sh
+++ create.sh
@@ -4,6 +4,6 @@ docker run --rm -it \
 	-w /go/src/${PWD#$GOPATH/src/} \
 	-e LOCAL_USER_ID=$(id -u) \
 	-e IN_DOCKER=1 \
-	zenoss/zenkit-build:1.1 \
+	zenoss/zenkit-build:1.3 \
 	/usr/local/bin/create-zenkit.sh $1
 (cd $1; make)
