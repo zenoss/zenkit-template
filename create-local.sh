5b25539ef386698363b5e38c12db89fb6da75491
Summer Mousa <smousa@zenoss.com>
2017-08-08 20:33:43 -0500

    bump the zenkit image again to handle non-interactive mode

--- create-local.sh
+++ create-local.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 : ${ROOTDIR:=$PWD/$1}
 
-docker run --rm -i \
+docker run --rm \
 	-v $GOPATH/src:/go/src \
 	-w /go/src/`dirname ${ROOTDIR#$GOPATH/src/}` \
 	-e LOCAL_USER_ID=$(id -u) \
 	-e IN_DOCKER=1 \
-	zenoss/zenkit-build:1.5 \
+	zenoss/zenkit-build:1.5.1 \
 	/usr/local/bin/create-zenkit-local.sh $1
 (cd $1; make)
