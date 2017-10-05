b120f23d559e679e5cae7b1c31b10ddd7ce4f13f
Summer Mousa <smousa@zenoss.com>
2017-10-05 16:18:03 -0500

    added a fixme to the dumb check

--- template/{{Name}}/cmd/healthcheck.go
+++ template/{{Name}}/cmd/healthcheck.go
@@ -9,6 +9,7 @@ import (
 	. "github.com/zenoss/zenkit/healthcheck/checks"
 )
 
+// FIXME: this is a contrived example, but others will be more useful
 func registerPing(port int, timeout, period time.Duration) {
 	r := fmt.Sprintf("http://localhost:%d/ping", port)
 	c := PeriodicChecker(HTTPChecker(r, 200, timeout, http.Header{}), period)
