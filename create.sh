7fef6c72c86361358a44aa81d1e833e2ef493dee
Danylo Kondratiev <knightpp@protonmail.com>
2022-06-28 21:46:34 +0300

    ZING-22426: add revive linter config (#75)

--- create.sh
+++ create.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 if docker run --rm -it \
-	-v $GOPATH/src:/go/src \
-	-w /go/src/${PWD#$GOPATH/src/} \
-	-e LOCAL_USER_ID=$(id -u) \
+	-v "$GOPATH"/src:/go/src \
+	-w /go/src/"${PWD#"$GOPATH"/src/}" \
+	-e LOCAL_USER_ID="$(id -u)" \
 	-e IN_DOCKER=1 \
-	zenoss/zenkit-build:1.10.0 \
-	/usr/local/bin/create-zenkit.sh $1
+	zenoss/zenkit-build:1.12.0 \
+	/usr/local/bin/create-zenkit.sh "$1"
 then
     echo "Complete. You should vendor dependencies with the following commands."
     echo
--- template/go.mod
+++ template/go.mod
@@ -4,8 +4,8 @@ go 1.17
 
 require (
 	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0
-	github.com/onsi/ginkgo/v2 v2.1.3
-	github.com/onsi/gomega v1.17.0
-	github.com/zenoss/zenkit/v5 v5.4.0
-	google.golang.org/grpc v1.43.0
+	github.com/onsi/ginkgo/v2 v2.1.4
+	github.com/onsi/gomega v1.19.0
+	github.com/zenoss/zenkit/v5 v5.6.1
+	google.golang.org/grpc v1.47.0
 )
