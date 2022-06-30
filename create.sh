f7af3cc42d81adeba34def397a74e3cf18d988db
Danylo Kondratiev <knightpp@protonmail.com>
2022-06-30 13:28:42 +0300

    Use zenkit-build:1.13.0 tag

--- create.sh
+++ create.sh
@@ -4,7 +4,7 @@ if docker run --rm -it \
 	-w /go/src/"${PWD#"$GOPATH"/src/}" \
 	-e LOCAL_USER_ID="$(id -u)" \
 	-e IN_DOCKER=1 \
-	zenoss/zenkit-build:1.12.0 \
+	zenoss/zenkit-build:1.13.0 \
 	/usr/local/bin/create-zenkit.sh "$1"
 then
     echo "Complete. You should vendor dependencies with the following commands."
