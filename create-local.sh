e710fbee7d6a5b76d4440dd14017710f6b3303f4
Danylo Kondratiev <knightpp@protonmail.com>
2022-12-14 15:06:00 +0200

    Fix scripts

--- create-local.sh
+++ create-local.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env bash
-: "${ROOTDIR:=$PWD/$1}"
 
-if docker run --rm \
-	-v "$GOPATH"/src:/go/src \
-	-w /go/src/"$(dirname "${ROOTDIR#"$GOPATH"/src/}")" \
-	-e LOCAL_USER_ID="$(id -u)" \
-	-e IN_DOCKER=1 \
-	zenoss/zenkit-build:1.13.0 \
-	/usr/local/bin/create-zenkit-local.sh "$1"
-then
-    echo "Complete. You should vendor dependencies with the following commands."
-    echo
-    echo "    cd $1"
-    echo "    make vendor"
-    echo
-fi
+set -uo pipefail
+
+# shellcheck source=create.sh
+source create.sh
+
+main() {
+	execute_template_in_docker "$@"
+}
+
+main "$1" "create-zenkit-local.sh"
