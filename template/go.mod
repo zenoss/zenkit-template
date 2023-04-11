f5cd841b2154835bcbd1226e5e327d3e7b03f303
Vladyslav Tatarin <48694776+VTGare@users.noreply.github.com>
2023-04-11 14:03:19 +0300

    ZING-27999: update to latest standards (#80)
    * update to latest standards

--- template/go.mod
+++ template/go.mod
@@ -1,11 +1,12 @@
 module github.com/zenoss/{{Name}}
 
-go 1.19
+go 1.20
 
 require (
 	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0
 	github.com/onsi/ginkgo/v2 v2.6.0
 	github.com/onsi/gomega v1.24.1
-	github.com/zenoss/zenkit/v5 v5.7.2
+	github.com/zenoss/zenkit/v5 v5.8.6
+	github.com/zenoss/zingo/v4 v4.6.0
 	google.golang.org/grpc v1.51.0
 )
--- template/main.go
+++ template/main.go
@@ -26,23 +26,26 @@ func main() {
 		log.WithError(err).Fatal("waiting for envoy failed")
 	}
 
-	err := zenkit.RunGRPCServer(ctx, ServiceName, func(svr *grpc.Server) error {
+	cancelInstrumentation, err := zenkit.InstallInstrumentation(ctx, log)
+	if err != nil {
+		log.WithError(err).Error("could not install instrumentation")
+	}
 
+	defer cancelInstrumentation()
+
+	err := zenkit.RunGRPCServer(ctx, ServiceName, func(svr *grpc.Server) error {
 		// Fill this in with your service details
 
 		// proto.Register{{Name | title}}ServiceServer(svr, New{{Name | title}}Service())
 
 		return nil
-
 	})
-
 	// Replace RunGRPCServer above with this if you're exposing a public API.
 	//
 	// err := zenkit.RunGRPCServerWithEndpoint(ctx, ServiceName, func(svr *grpc.Server) error {
 	// 	proto.Register{{Name | title}}ServiceServer(svr, serviceServer)
 	// 	return nil
 	// }, proto.Register{{Name | title}}ServiceHandlerFromEndpoint)
-
 	if err != nil {
 		log.WithError(err).Fatal("error running gRPC server")
 	}
